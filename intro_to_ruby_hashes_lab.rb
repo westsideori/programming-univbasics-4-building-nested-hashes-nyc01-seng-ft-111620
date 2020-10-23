33dcb97de47357863f7a52ebc69fe252d12dd243
inventive-view-7245 <ori.markowitz@gmail.com>
2020-10-23 16:43:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,8 @@
 def base_hash
-  hash = {railroads: {}}
+  {railroads: {}}
 end
 
 def monopoly_with_second_tier
-  hash = {railroads: {}}
-  hash[:railroads]
-  hash[:railroads][:pieces] = 4
-  [:railroads][:rent_in_dollars] = {}
-  [:railroads][:names] = {}
-end
-
-def monopoly_with_third_tier
   {
     railroads: 
     {
@@ -19,13 +11,55 @@ def monopoly_with_third_tier
       names: {}
     }
   }
+end
+
+def monopoly_with_third_tier
+  {
+    railroads: 
+    {
+      pieces: 4, 
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_piece_owned: 50,
+        three_piece_owned: 100,
+        four_piece_owned: 200
+      },
+      names: {
+        reading_railroad: {},
+        pennsylvania_railroad: {},
+        b_and_o_railroad: {},
+        shortline_railroad: {}
+      }
+    }
+  }
 
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  {
+    railroads: 
+    {
+      pieces: 4, 
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_piece_owned: 50,
+        three_piece_owned: 100,
+        four_piece_owned: 200
+      },
+      names: {
+        reading_railroad: {
+          mortgage_value: 100,
+        },
+        pennsylvania_railroad: {
+          mortgage_value: 200
+        },
+        b_and_o_railroad: {
+          mortgage_value: 400
+        },
+        shortline_railroad: {
+          mortgage_value: 800
+        }
+      }
+    }
 
 end
